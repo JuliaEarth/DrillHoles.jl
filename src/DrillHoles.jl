1a96c0d46685e53e7578ddf57f8d3464a2d184eb
rmcaixeta <8386288+rmcaixeta@users.noreply.github.com>
2020-12-02 10:49:39 -0300

    Compositing running, but not validated

--- src/DrillHoles.jl
+++ src/DrillHoles.jl
@@ -2,11 +2,11 @@ module DrillHoles
 
 using CSV
 using DataFrames
+using StatsBase:mean,weights
 
 include("desurvey.jl")
 include("mergetables.jl")
-include("compositing.jl")
-
+include("compositing.jl")]
 
 export
     Collar,
@@ -14,23 +14,5 @@ export
     IntervalTable,
     drillhole,
     composite
-
-
-
-"""
-## Workflow draft
-
-using DrillHoles
-fp = "/home/rafa/Documents/programming/julialang/dh_functions/"
-collar = Collar(file=string(fp,"collar.csv"),holeid="BHID",x="XCOLLAR",y="YCOLLAR",z="ZCOLLAR",enddepth="ENDDEPTH")
-survey = Survey(file=string(fp,"survey.csv"),holeid="BHID",at="AT",azm="BRG",dip="DIP",invertdip=false)
-litho = IntervalTable(file=string(fp,"litho.csv"),holeid="BHID",from="FROM",to="TO")
-assay = IntervalTable(file=string(fp,"assay.csv"),holeid="BHID",from="FROM",to="TO")
-
-dh = drillhole(collar,survey,[litho,assay])
-dhcomp = composite(dh)
-
-"""
-
 
 end
--- src/compositing.jl
+++ src/compositing.jl
@@ -1,44 +1,114 @@
 
 """
-nan = zero or absent
+nan = :zero or skip
 gaps = 0
 mincomp
 interval
-maxcomp = 1.5*interval; equal to interval
+aim = :equalcomp or :nodiscard
+density weighted?
+core recovery?
 """
 
 function composite(dhf::DrillHole; interval::Number=1.0,
-  zone=nothing, gap=0.001, mincomp=0.5, maxcomp="interval")
+  zone=nothing, gap=0.001, mincomp=0.5, aim=:equalcomp)
 
-  dh = dhf.table
+  dh = copy(dhf.table)
   codes = dhf.codes
   bh, from, to = codes.holeid, codes.from, codes.to
 
   # get groups to composite
-  shift = collect(2:size(dh,1))
-  push!(shift,1) # check if works for every case
-
-  chk1 = abs.(dh[shift,to] .- dh[!,from]) .> gap # gap
-  chk2 = dh[shift,bh] .!= dh[!,bh] # hole name
-  chk3 = zone == nothing ? nothing : (dh[shift,zone] .!= dh[!,zone]) # zone check
-
-  breaks = zone == nothing ? (chk1 .| chk2) : (chk1 .| chk2 .| chk3)
-  println(chk1[1:25])
-  println(chk2[1:25])
-  println(chk3)
-  println(breaks[1:25])
-
   gps = []; c = 1
   for i in 1:size(dh,1)
-    breaks[i] && i > 1 && (c += 1)
+    i == 1 && push!(gps,c)
+    i == 1 && continue
+
+    t1 = (dh[i-1,to] - dh[i,from]) > gap
+    t2 = dh[i-1,bh] != dh[i,bh]
+    t3 = zone == nothing ? false : dh[i-1,zone] != dh[i,zone]
+    (t1 || t2 || t3) && (c += 1)
     push!(gps,c)
   end
 
-  chk = select(dh,[bh,from,to])
-  chk[!,"GP"] = gps
-  CSV.write("test_groups.csv",chk)
+  maincols = Symbol.([bh,from,to,:LENGTH])
+  zone != nothing && push!(maincols,Symbol(zone))
+  num = eltype.(eachcol(dh)) .<: Union{Missing, Number}
+  numcols = setdiff(Symbol.(names(dh))[num],maincols)
+  numcols = setdiff(numcols,[:X,:Y,:Z])
+  select!(dh,union(maincols,numcols))
 
-  """
+  comps = DataFrame() #copy(first(dh,0))
+  dh[!,"_GP_"] = gps
+
+  for dft in groupby(dh,:_GP_)
+    len = sum(dft[!,:LENGTH])
+  	len < mincomp && continue
+    dfa = DataFrame()
+
+    if aim == :nodiscard
+      div = len/interval
+      if div <= 1 || isinteger(div)
+        nbint = ceil(Int,div)
+      else
+        prenbint = [floor(Int,div), ceil(Int,div)]
+        closest = argmin([abs(interval-len/x) for x in prenbint])
+        nbint = prenbint[closest]
+      end
+
+      intlen = nbint > 1 ? len/nbint : len
+      minfrom = minimum(dft[!,:FROM])
+      maxto = maximum(dft[!,:TO])
+
+      dfa[!,from] = collect(minfrom:intlen:maxto)[1:nbint]
+      dfa[!,to] = dfa[!,from] .+ intlen
+      dfa[!,bh] .= dft[1,bh]
+      dfa[!,:LENGTH] .= intlen
+      zone != nothing && (dfa[!,zone] .= dft[1,zone])
+    else
+      div = len/interval
+      nbint = floor(Int,div)
+      last = (len-nbint*interval)
+      last >= mincomp && (nbint += 1)
+      last < mincomp && (last = interval)
+
+      minfrom = minimum(dft[!,:FROM])
+      maxto = maximum(dft[!,:TO])
+
+      dfa[!,from] = collect(minfrom:interval:maxto)[1:nbint]
+      dfa[!,to] = dfa[!,from] .+ interval
+      dfa[end,to] -= (interval-last)
+      dfa[!,bh] .= dft[1,bh]
+      dfa[!,:LENGTH] = dfa[!,to] - dfa[!,from]
+      zone != nothing && (dfa[!,zone] .= dft[1,zone])
+    end
+
+    # initialize new cols
+    for col in numcols
+      dfa[!,col] .= -9999.9999
+    end
+    allowmissing!(dfa, numcols)
+
+    # averaging numeric values
+    wcols = union(Symbol.([from,to]), numcols)
+    for i in 1:size(dfa,1)
+      f, t = dfa[i,from], dfa[i,to]
+      vals = dft[(dft[!,to] .> f) .& (dft[!,from] .< t), wcols]
+      size(vals,1) == 0 && (dfa[i,col] = missing)
+      size(vals,1) == 0 && continue
+      vals[1,from]=f
+      vals[end,to]=t
+      leng = vals[:,to]-vals[:,from]
+      for col in numcols
+        ivalid = findall(!ismissing,vals[!,col])
+        n = length(ivalid)
+        wgt = n >= 1 ? weights(leng[ivalid]) : missing
+        n == 0 && (dfa[i,col] = missing)
+        n == 1 && (dfa[i,col] = vals[1,col])
+        n > 1 && (dfa[i,col] = mean(vals[ivalid,col], wgt))
+      end
+    end
+    comps = vcat(comps, dfa, cols=:union)
+  end
+
+  fillxyz!(comps, dhf.trace, codes)
   DrillHole(dhf.trace,comps,codes)
-  """
 end
